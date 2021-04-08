34845f285fad31b2bc72976bb210069e091a0daa
Niaso <39199340+Zpk2@users.noreply.github.com>
2021-04-08 18:20:18 +0200

    🚧 Commit

---
+++ Easy Webhooks.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Easy_Webhooks
+{
+    partial class EasyWebhooks
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EasyWebhooks));
+            this.sendbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lienwebhook = new System.Windows.Forms.TextBox();
+            this.nomwebhook = new System.Windows.Forms.TextBox();
+            this.message = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendbtn
+            // 
+            this.sendbtn.Location = new System.Drawing.Point(472, 144);
+            this.sendbtn.Name = "sendbtn";
+            this.sendbtn.Size = new System.Drawing.Size(107, 46);
+            this.sendbtn.TabIndex = 0;
+            this.sendbtn.Text = "Envoyer";
+            this.sendbtn.UseVisualStyleBackColor = true;
+            this.sendbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(532, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(47, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Mon site";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // lienwebhook
+            // 
+            this.lienwebhook.Location = new System.Drawing.Point(141, 52);
+            this.lienwebhook.Name = "lienwebhook";
+            this.lienwebhook.Size = new System.Drawing.Size(289, 20);
+            this.lienwebhook.TabIndex = 3;
+            // 
+            // nomwebhook
+            // 
+            this.nomwebhook.Location = new System.Drawing.Point(141, 78);
+            this.nomwebhook.MaxLength = 20;
+            this.nomwebhook.Name = "nomwebhook";
+            this.nomwebhook.Size = new System.Drawing.Size(289, 20);
+            this.nomwebhook.TabIndex = 4;
+            this.nomwebhook.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(141, 104);
+            this.message.MaxLength = 2000;
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(289, 20);
+            this.message.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Lien du Webhook :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nom du Webhook :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(200, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(178, 28);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Easy Webhooks";
+            // 
+            // EasyWebhooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 202);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.nomwebhook);
+            this.Controls.Add(this.lienwebhook);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendbtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "EasyWebhooks";
+            this.Text = "Easy Webhooks";
+            this.Load += new System.EventHandler(this.EasyWebhooks_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox lienwebhook;
+        private System.Windows.Forms.TextBox nomwebhook;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Easy Webhooks.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Easy_Webhooks
+{
+    public partial class EasyWebhooks : Form
+    {
+        public EasyWebhooks()
+        {
+            InitializeComponent();
+        }
+        public static void sendWebhook(string Url, string msg, string Username)
+        {
+            Http.Post(Url, new System.Collections.Specialized.NameValueCollection()
+            {
+                {
+                    "username",
+                    Username
+                },
+                {
+                    "content",
+                    msg
+                }
+            });
+        }
+        private void EasyWebhooks_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://niaso.cf");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string webhlink = lienwebhook.Text;
+            string messagetxt = message.Text;
+            string name = nomwebhook.Text;
+
+            if (message.Text == "")
+                MessageBox.Show("Entre un nom");
+
+            else if (message.Text == "")
+                MessageBox.Show("Entre un message");
+
+            else {
+
+            sendWebhook(lienwebhook.Text, message.Text, nomwebhook.Text);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendembed_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
